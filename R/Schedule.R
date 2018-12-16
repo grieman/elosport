2bdbdcf30bec18b074bc5f09f05487b1b292c7f2
Graeham Rieman <graeham.rieman@gmail.com>
2018-12-16 16:57:36 -0500

    First pass at 'Schedule' class

---
+++ R/Schedule.R
@@ -0,0 +1,28 @@
+Schedule <- R6::R6Class("Schedule",
+      public = list(
+        year = NULL,
+        df = NULL,
+        elo_params = NULL,
+        games = NULL,
+        initialize = function(year, source_df, elo_params){
+          self$year <- year
+          self$df <- source_df
+          self$elo_params <- elo_params
+        },
+        make_games = function(all_teams){
+          self$games <- list()
+          ## Iterate through df and make a game for each line
+          for (i in 1:nrow(self$df)) {
+            row <- self$df[i,]
+            self$games[[paste(row$Home_Team,
+                              row$Away_Team,
+                              row$Date,
+                              sep = '_')]] <- Game(row$Date,
+                                                  row$Home_Team,
+                                                  row$Away_Team,
+                                                  row$Home_Score,
+                                                  row$Away_Score,
+                                                  self$elo_params)
+          }
+        }
+      ))
